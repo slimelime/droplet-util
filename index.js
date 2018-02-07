661974365e25a1542d0ff323a556c702470d7bc7
Thinh Nguyen <Thinh.Nguyen@myob.com>
2018-02-08 10:02:11 +1100

    export more libs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const LOGGER = require('./lib/logger');
-
 module.exports = {
-    LOGGER
+    logger: require('./lib/logger'),
+    propertyValidator: require('./lib/property-validator'),
+    idamTokenValidator: require('./lib/idam-token-validator'),
+    datetimeProvider: require('./lib/datetime-provider'),
+    jsonTransformer: require('./lib/json-transformer'),
+    kms: require('./lib/aws/kms')
 };
