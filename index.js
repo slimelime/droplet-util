53fd72572be9eb49eb5f6aa337d413e85ea0a0c9
Suppachart Tantisureeporn <suppachart.tantisureeporn@myob.com>
2018-08-07 11:26:53 +1000

    Remove aws2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
     aws: {
         dynamo: require('./lib/aws/dynamo')
     },
-    aws2: require('./lib/aws'),
     logger: require('./lib/logger'),
     propertyValidator: require('./lib/property-validator'),
     idamTokenValidator: require('./lib/idam-token-validator'),
