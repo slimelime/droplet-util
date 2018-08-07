8a07268a4463eeb9d5633c7e3dcaeb503e2ec394
Suppachart Tantisureeporn <suppachart.tantisureeporn@myob.com>
2018-08-07 11:22:33 +1000

    export functions

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = {
+    aws: {
+        dynamo: require('./lib/aws/dynamo')
+    },
+    aws2: require('./lib/aws'),
     logger: require('./lib/logger'),
     propertyValidator: require('./lib/property-validator'),
     idamTokenValidator: require('./lib/idam-token-validator'),
